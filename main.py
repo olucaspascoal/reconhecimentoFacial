e7f14bc54c58f6eecffdaa0cd35e674b3bfc0c9b
Lucas Pascoal <olucaspascoal@gmail.com>
2022-02-27 20:29:48 -0300

    primeira versão do reconhecimento facial

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+
+# Puxando a imagem da webcam
+videoCapture = cv2.VideoCapture(0)
+
+# Identificando um rosto
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+
+while True:
+    ret, frame = videoCapture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    eyes = eyeCascade.detectMultiScale(gray, 1.2, 18)
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    smile = smileCascade.detectMultiScale(gray, 1.7, 20)
+    for (x, y, w, h) in smile:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # Exibindo a webcam na tela
+    cv2.imshow('webcam', frame)
+
+    # O programa só encerra se a tecla 'Q' for pressionada
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+videoCapture.release()
+cv2.destroyAllWindows()
